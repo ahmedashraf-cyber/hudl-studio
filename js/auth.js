ef2fd53077fe8b8b8d8b095488d7a64dbf3cb8ad
ahmedashraf-cyber <ahmed.ashraf@hudl.com>
2026-05-02 12:44:51 +0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,40 @@
+import { auth } from './firebase-config.js';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+export function onAuthChanged(callback) {
+  return onAuthStateChanged(auth, callback);
+}
+
+export async function signUp(email, password, displayName) {
+  const cred = await createUserWithEmailAndPassword(auth, email, password);
+  await updateProfile(cred.user, { displayName });
+  return cred.user;
+}
+
+export async function signIn(email, password) {
+  const cred = await signInWithEmailAndPassword(auth, email, password);
+  return cred.user;
+}
+
+export async function signInGoogle() {
+  const cred = await signInWithPopup(auth, provider);
+  return cred.user;
+}
+
+export async function logout() {
+  await signOut(auth);
+}
+
+export function currentUser() {
+  return auth.currentUser;
+}
